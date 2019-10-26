16fa2a05f7cea2cc267e5f47a52b32b9b006abf2
sangeet <sangeet.khatri@protonmail.com>
2019-10-26 20:01:44 +0530

    Final changes for favicon, title and file size optimisations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + TailwindCSS + Netlify CMS Starter",
+    title: "The Silent Cow",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Delivering a fine tuned method to solving a cube that’s fun for the young & teaches professionals how to tackle the puzzles in their work life.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/img/logo-cow.png",
+      },
+    },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
@@ -77,7 +83,8 @@ module.exports = {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+        purgeOnly: ["/all.sass", "/tailwind.css"], // applies purging only on the bulma css file
+        tailwind: true,
       },
     }, // must be after other CSS plugins
     "gatsby-plugin-netlify", // make sure to keep it last in the array
