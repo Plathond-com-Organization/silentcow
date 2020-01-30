bd19b86c06e0f84c347aaab36bb29b36f1988771
David Plews <cleargif@users.noreply.github.com>
2020-01-30 12:25:14 +0000

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,14 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import showdown from "showdown"
 
 const FeatureGrid = ({ gridItems }) => {
+  const converter = new showdown.Converter()
   return (
     <div className="flex flex-wrap -mx-5">
       {gridItems.map(item => (
         <div key={item.text} className="p-5 flex flex-col md:w-1/2">
           <PreviewCompatibleImage imageInfo={item}/>
           <span className="mt-4">{item.text}</span>
+          <span className="mt-4 markdown" dangerouslySetInnerHTML={{ __html: converter.makeHtml(item.text) }}></span>
         </div>
       ))}
     </div>
