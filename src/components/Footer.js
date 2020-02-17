587662f7574a1252f22310b518bf9e8920b31a82
David Plews <cleargif@users.noreply.github.com>
2020-02-17 17:09:44 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,7 @@ const Footer = () => {
         <div className="md:w-1/3 flex md:flex-col self-stretch justify-center md:justify-between items-end text-white text-5xl md:text-3xl">
           {social.map((obj, index) => (
             <a href={obj.link} target="_blank">
-              <i className="m-3 md:m-1 fab fa-" + {obj.icon}></i>
+              <i className={obj.icon}></i>
             </a>
           ))}
         </div>
